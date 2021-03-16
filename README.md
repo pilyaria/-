cfc9a7588416b5bbd45aae0f7830609caab894b4
pilyaria <pilyar@teplo-inform.ru>
2021-03-16 11:07:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-bqnc33-6mthn5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-bqnc33-6mthn5)
---
+++ index.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Table } from 'antd';
+import { sortableContainer, sortableElement, sortableHandle } from 'react-sortable-hoc';
+import { MenuOutlined } from '@ant-design/icons';
+import arrayMove from 'array-move';
+
+const DragHandle = sortableHandle(() => (
+  <MenuOutlined style={{ cursor: 'pointer', color: '#999' }} />
+));
+
+const columns = [
+  {
+    title: 'Sort',
+    dataIndex: 'sort',
+    width: 30,
+    className: 'drag-visible',
+    render: () => <DragHandle />,
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    className: 'drag-visible',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+  },
+
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'Ведомости',
+    age: 32,
+    
+    index: 0,
+  },
+  {
+    key: '2',
+    name: 'Настройки',
+    age: 42,
+    index: 1,
+  },
+  {
+    key: '3',
+    name: 'Графики',
+    age: 32,
+    index: 2,
+  },
+    {
+    key: '4',
+    name: 'Кадр',
+    age: 32,
+    index: 3,
+  },
+];
+
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+  },
+  getCheckboxProps: (record) => ({
+    disabled: record.name === 'Настройки',
+    // Column configuration not to be checked
+    name: record.name,
+  }),
+};
+
+const SortableItem = sortableElement(props => <tr {...props} />);
+const SortableContainer = sortableContainer(props => <tbody {...props} />);
+
+class SortableTable extends React.Component {
+  state = {
+    dataSource: data,
+  };
+
+  onSortEnd = ({ oldIndex, newIndex }) => {
+    const { dataSource } = this.state;
+    if (oldIndex !== newIndex) {
+      const newData = arrayMove([].concat(dataSource), oldIndex, newIndex).filter(el => !!el);
+      console.log('Sorted items: ', newData);
+      this.setState({ dataSource: newData });
+    }
+  };
+
+  DraggableContainer = props => (
+    <SortableContainer
+      useDragHandle
+      disableAutoscroll
+      helperClass="row-dragging"
+      onSortEnd={this.onSortEnd}
+      {...props}
+    />
+  );
+
+  DraggableBodyRow = ({ className, style, ...restProps }) => {
+    const { dataSource } = this.state;
+    // function findIndex base on Table rowKey props and should always be a right array index
+    const index = dataSource.findIndex(x => x.index === restProps['data-row-key']);
+    return <SortableItem index={index} {...restProps} />;
+  };
+
+  render() {
+    const { dataSource } = this.state;
+
+    return (
+      <Table
+       
+        dataSource={dataSource}
+        columns={columns}
+        rowKey="index"
+        components={{
+          body: {
+            wrapper: this.DraggableContainer,
+            row: this.DraggableBodyRow,
+          },
+        }}
+      />
+    );
+  }
+}
+
+
+
+ReactDOM.render(<SortableTable />, document.getElementById('container'));
